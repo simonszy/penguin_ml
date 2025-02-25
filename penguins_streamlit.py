811f34a894da823a6ae6744a12904d0e7eb47f00
simons <simons@simons.com>
2025-02-25 17:03:45 -0500

    add secs

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -18,7 +18,8 @@ st.write(
 )
 
 password_guess = st.text_input("What is the Password?")
-if password_guess != "streamlit_is_great":
+if password_guess != st.secrets('password'):
+    st.write("Incorrect Password, please try again.")
     st.stop()
 
 
